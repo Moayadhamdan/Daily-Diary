71a0c2d7b5ca81002d7658c74238b96dab5622a0
MOAYAD HAMDAN <23040732@student.ltuc.com>
2024-06-26 23:17:51 +0300

    Add project files.

---
+++ DiaryManager/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DiaryManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filepath = Path.Combine(Environment.CurrentDirectory, "mydiary.txt");
+        }
+    }
+}
